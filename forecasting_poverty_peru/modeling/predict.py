bbe404e58c2b6cd5d736000f1fd14995e092af63
Eduardo Castro <eduardo.castro@utec.edu.pe>
2025-02-11 08:31:51 +0000

    feat(training): add batch size finder

--- forecasting_poverty_peru/modeling/predict.py
+++ forecasting_poverty_peru/modeling/predict.py
@@ -7,11 +7,12 @@ from torch.utils.data import DataLoader, random_split
 from torchvision.datasets import DatasetFolder
 from torchvision.transforms import v2
 import lightning as L
+from lightning.pytorch.tuner import Tuner
 from lightning.pytorch.loggers import TensorBoardLogger
 from PIL import Image
 
-from poverty_forecasting_peru.config import MODELS_PATH, PROCESSED_PATH
-from poverty_forecasting_peru.modeling.satmae_pp.fmow_rgb.models_vit import vit_large_patch16
+from forecasting_poverty_peru.config import MODELS_PATH, PROCESSED_PATH
+from forecasting_poverty_peru.modeling.satmae_pp.fmow_rgb.models_vit import vit_large_patch16
 
 SIENAHO_PATH = PROCESSED_PATH / 'sienaho_rgb'
 
@@ -25,10 +26,11 @@ CHECKPOINT_FILEPATH = (
 
 
 class LitFinetuneViTL(L.LightningModule):
-    def __init__(self, vit):
+    def __init__(self, vit, batch_size=1):
         super().__init__()
         self.vit = vit
         self.criterion = nn.CrossEntropyLoss()
+        self.batch_size = batch_size
 
     def forward(self, x):
         return self.vit(x)
@@ -38,6 +40,9 @@ class LitFinetuneViTL(L.LightningModule):
         scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=5)
         return [optimizer], [scheduler]
 
+    def train_dataloader(self):
+        return DataLoader(dataset=datasets['train'], batch_size=self.batch_size, num_workers=7)
+
     def training_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self.forward(x)
@@ -45,12 +50,18 @@ class LitFinetuneViTL(L.LightningModule):
         self.log('train_loss', loss, on_epoch=True, prog_bar=True, logger=True)
         return loss
 
+    def val_dataloader(self):
+        return DataLoader(dataset=datasets['val'], batch_size=self.batch_size, num_workers=7)
+
     def validation_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self.forward(x)
         loss = self.criterion(y_hat, y)
         self.log('val_loss', loss, on_epoch=True, prog_bar=True, logger=True)
         return loss
+
+    def test_dataloader(self):
+        return DataLoader(dataset=datasets['test'], batch_size=self.batch_size, num_workers=7)
 
     def test_step(self, batch, batch_idx):
         x, y = batch
@@ -81,7 +92,8 @@ model.head = nn.Linear(
 
 transform = v2.Compose([
     v2.RandomCrop((224, 224)),
-    v2.PILToTensor()
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True)
 ])
 
 dataset = DatasetFolder(
@@ -89,21 +101,37 @@ dataset = DatasetFolder(
     loader=Image.open,
     extensions=['.tif'],
     transform=transform,
-    target_transform=lambda x: x == 'adequate'
+    target_transform=lambda x: 1 if x == 'adequate' else 0
 )
 
 dataset_size = len(dataset)
-train_size = int(0.7 * dataset_size)
-val_size = int(0.2 * dataset_size)
-test_size = dataset_size - train_size - val_size
+
+sizes = {}
+
+sizes['train'] = int(0.7 * dataset_size)
+sizes['val'] = int(0.2 * dataset_size)
+sizes['test'] = dataset_size - sizes['train'] - sizes['val']
 
 seed = torch.Generator().manual_seed(42)
 
-train_dataset, val_dataset, test_dataset = random_split(
+datasets = {}
+
+datasets['train'], datasets['val'], datasets['test'] = random_split(
     dataset,
-    [train_size, val_size, test_size],
+    sizes.values(),
     generator=seed
 )
+
+num_workers = 7
+
+loader = {
+    split: DataLoader(
+        datasets[split],
+        batch_size=32,
+        shuffle=split == 'train',
+        num_workers=num_workers
+    ) for split in ['train', 'val', 'test']
+}
 
 lit_model = LitFinetuneViTL(model)
 
@@ -114,6 +142,10 @@ trainer = L.Trainer(
     logger=logger,
 )
 
-trainer.fit(lit_model, DataLoader(train_dataset), DataLoader(val_dataset))
+tuner = Tuner(trainer)
 
-trainer.test(lit_model, DataLoader(test_dataset))
+tuner.scale_batch_size(lit_model, mode='power')
+
+trainer.fit(lit_model, loader['train'], loader['val'])
+
+trainer.test(lit_model, loader['test'])
